33688505a8299d35ab3d5576c71f23b5d94934cc
josephprasadula <josephprasadula@gmail.com>
2022-06-29 18:04:39 +0530

    small adjustamnets again

--- pages/src/components/frequently.js
+++ pages/src/components/frequently.js
@@ -4,9 +4,7 @@ import { Collapse, Select } from 'antd';
 import { useState } from 'react';
 const { Panel } = Collapse;
 const { Option } = Select;
-const text = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu tristique nibh, eu scelerisque sem. 
-`;
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu tristique nibh, eu scelerisque sem.`;
 
 const onChange = (key) => {
     console.log(key);
@@ -30,21 +28,21 @@ export default function Frequently() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu tristique nibh, eu scelerisque sem. </p>
             </div>
             <div className='accordian'>
-            <Collapse accordion bordered={false} ghost defaultActiveKey={['1']} onChange={onChange} >
+            <Collapse accordion style={{backgroundColor:'white'}} bordered={false}  defaultActiveKey={['1']} onChange={onChange} >
             <Panel className='panel' showArrow={false} header={<h3>Lorem ipsum dolor sit amet?</h3>} key="1" extra={genExtra()}>
-                <p>{text}</p>
+                <p className='accordiantext'>{text}</p>
             </Panel>
             <Panel className='panel' showArrow={false} header={<h3>Proin eu tristique nibh, eu scelerisque se?</h3>} key="2" extra={genExtra()}>
-                <p>{text}</p>
+                <p className='accordiantext'>{text}</p>
             </Panel>
             <Panel className='panel' showArrow={false} header={<h3>Maecenas placerat dui quis imperdiet?</h3>} key="3" extra={genExtra()}>
-                <p>{text}</p>
+                <p className='accordiantext'>{text}</p>
             </Panel>
             <Panel className='panel' showArrow={false} header={<h3>Quisque condimentum sit amet sapien et?</h3>} key="4" extra={genExtra()}>
-                <p>{text}</p>
+                <p className='accordiantext'>{text}</p>
             </Panel>
             <Panel className='panel' showArrow={false} header={<h3>Nam magna massa, consequat eu luctus ut?</h3>} key="5" extra={genExtra()}>
-                <p>{text}</p>
+                <p className='accordiantext'>{text}</p>
             </Panel>
             </Collapse>
             </div>
