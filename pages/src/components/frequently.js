0f21746b80d6af8172ba8807ae7f84c87165d984
josephprasadula <josephprasadula@gmail.com>
2022-07-01 18:20:34 +0530

    change in frequent section

--- pages/src/components/frequently.js
+++ pages/src/components/frequently.js
@@ -2,14 +2,18 @@ import React from 'react'
 import {PlusOutlined ,CloseCircleTwoTone } from '@ant-design/icons';
 import { Collapse, Select } from 'antd';
 import { useState } from 'react';
+
 const { Panel } = Collapse;
 const { Option } = Select;
 const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu tristique nibh, eu scelerisque sem.`;
 
+
+export default function Frequently() {
+    const [className,setClassName] = useState('panel');
 const onChange = (key) => {
     console.log(key);
-};
-export default function Frequently() {
+    setClassName('box-open')
+};    
   return (
     <section className='frequentlysection'>
         <div className='container'>
@@ -19,20 +23,20 @@ export default function Frequently() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu tristique nibh, eu scelerisque sem. </p>
             </div>
             <div className='accordian'>
-            <Collapse expandIcon={({ isActive }) => isActive ? <CloseCircleTwoTone style={{fontSize:'2rem'}} /> : <PlusOutlined style={{fontSize: '24px',borderRadius: '50%',backgroundColor: '#eff0f6',padding: '4px'}}/>} expandIconPosition='end' accordion style={{backgroundColor:'white'}} bordered={false}  defaultActiveKey={['1']} onChange={onChange} collapsible='icon'>
-            <Panel className='panel' header={<h3>Lorem ipsum dolor sit amet?</h3>} key="1">
+            <Collapse className='accordian-collapse' expandIcon={({ isActive }) => isActive ? <CloseCircleTwoTone style={{fontSize:'2rem'}} /> : <PlusOutlined style={{fontSize: '24px',borderRadius: '50%',backgroundColor: '#eff0f6',padding: '4px'}}/>} expandIconPosition='end' accordion style={{backgroundColor:'white'}} bordered={false}  defaultActiveKey={['1']} onChange={onChange} collapsible='icon'>
+            <Panel className={className} header={<h3>Lorem ipsum dolor sit amet?</h3>} key="1">
                 <p className='accordiantext'>{text}</p>
             </Panel>
-            <Panel className='panel' header={<h3>Proin eu tristique nibh, eu scelerisque se?</h3>} key="2">
+            <Panel className={className} header={<h3>Proin eu tristique nibh, eu scelerisque se?</h3>} key="2">
                 <p className='accordiantext'>{text}</p>
             </Panel>
-            <Panel className='panel' header={<h3>Maecenas placerat dui quis imperdiet?</h3>} key="3">
+            <Panel className={className} header={<h3>Maecenas placerat dui quis imperdiet?</h3>} key="3">
                 <p className='accordiantext'>{text}</p>
             </Panel>
-            <Panel className='panel' header={<h3>Quisque condimentum sit amet sapien et?</h3>} key="4">
+            <Panel className={className} header={<h3>Quisque condimentum sit amet sapien et?</h3>} key="4">
                 <p className='accordiantext'>{text}</p>
             </Panel>
-            <Panel className='panel' header={<h3>Nam magna massa, consequat eu luctus ut?</h3>} key="5">
+            <Panel className={className} header={<h3>Nam magna massa, consequat eu luctus ut?</h3>} key="5">
                 <p className='accordiantext'>{text}</p>
             </Panel>
             </Collapse>
