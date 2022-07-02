6a8fcda934d06cf3d8643c0a881c39b6d11c8bd7
josephprasadula <josephprasadula@gmail.com>
2022-07-02 18:18:00 +0530

    compleated

--- pages/src/components/Footer.js
+++ pages/src/components/Footer.js
@@ -20,18 +20,18 @@ export default function Footer() {
                 <h4 className='footer-text'>FAQ</h4>
                 <h4 className='footer-text'>Download</h4>
             </Col>
-            <Col lg={{span:4}} xs={{span:24}}>
-                <Row justify='center' gutter={4}>
-                    <Col><FacebookFilled style={{fontSize:'1.5rem',marginTop:'1.5rem'}} /></Col>
-                    <Col><TwitterOutlined style={{fontSize:'1.5rem',marginTop:'1.5rem'}} /></Col>
-                    <Col><InstagramOutlined style={{fontSize:'1.5rem',marginTop:'1.5rem'}} /></Col>            
+            <Col lg={{span:5}} xs={{span:24}}>
+                <Row justify='center' className='icon-box'>
+                    <Col><FacebookFilled style={{fontSize:'1.5rem',margin:'0rem 1rem'}} /></Col>
+                    <Col><TwitterOutlined style={{fontSize:'1.5rem',margin:'0rem 1rem'}} /></Col>
+                    <Col><InstagramOutlined style={{fontSize:'1.5rem',margin:'0rem 1rem'}} /></Col>            
                 </Row>
             </Col>
-            <Col lg={{span:2,offset:2}} xs={{span:0}}>
+            <Col lg={{span:2,offset:1}} xs={{span:0}}>
             <Button type="primary" onClick={()=>{document.documentElement.scrollTop = 0;}} shape="circle" icon={<ArrowUpOutlined />} size='large' />
             </Col>
         </Row>
-        <Row>
+        <Row style={{marginTop:'2rem'}}>
             <Col lg={{span:16,offset:0}} xs={{span:20,offset:2}}><h4 className='footer-text'>©2021 QuadReal+. All Rights Reserved.</h4></Col>
             <Col lg={{span:3,offset:1}} xs={{span:0}}><h4 className='footer-text'>Terms & Conditions</h4></Col>
             <Col lg={{span:3,offset:1}} xs={{span:0}}><h4 className='footer-text'>Privacy policy</h4></Col>
